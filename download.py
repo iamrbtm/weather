532864bc7084295cff0ee64337c7f81ebe1ef24e
iamrbtm <rbtm2006@me.com>
2020-12-25 03:23:03 -0800

    commit

--- download.py
+++ download.py
@@ -1,17 +1,19 @@
 from io import BytesIO
 from PIL import Image
 import requests
+import os
 
-files = ['01d', '01n', '02d', '02n', '03d', '03n', '04d', '04n', '09d', '09n', '10d', '11d', '11n', '13d', '13n', '50d', '50n',]
+files = ['01d', '01n', '02d', '02n', '03d', '03n', '04d', '04n', '09d',
+         '09n', '10d', '10n', '11d', '11n', '13d', '13n', '50d', '50n', ]
+exts = ['.png', '@2x.png']
 url = 'https://openweathermap.org/img/wn/'
 
 for file in files:
-    r = requests.get(url + file + '@2x.png')
-    im = Image.open(BytesIO(r.content))
-    im.save('forcast/' + file + '@2x.png')
-
-    r = requests.get(url + file + '.png')
-    im = Image.open(BytesIO(r.content))
-    im.save('forcast/' + file + '.png')
-    
-    print ('Done with ' + file)
+    for ext in exts:
+        if not os.path.isfile(os.path.join(os.getcwd(), 'forcast/', file + ext)):
+            r = requests.get(url + file + ext)
+            im = Image.open(BytesIO(r.content))
+            im.save('forcast/' + file + ext)
+            print('Downloaded ' + file + ext)
+        else:
+            print(file + ext + ' already downloaded')
